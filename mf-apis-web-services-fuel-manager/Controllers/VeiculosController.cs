eff4a21882537c492735996e3be252641759acaa
eduardocesl <eduardocesl@outlook.com>
2024-09-23 14:03:33 -0300

    Criação do Controller Usuario e Criptografia da Senha

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -26,25 +26,26 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         [HttpPost]
         public async Task<ActionResult> Create(Veiculo model)
         {
-            if(model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
             {
-                return BadRequest(new {message = "Ano de Fabricação e Ano do Modelo são obrigatórios e devem ser maiores do que zero"});
+                return BadRequest(new { message = "Ano de Fabricação e Ano do Modelo são obrigatórios e devem ser maiores do que zero" });
             }
 
             _context.Veiculos.Add(model);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetById", new {id = model.Id}, model);
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult> GetById(int id)
         {
             var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t => t.Usuario)
                 .Include(t => t.Consumos)
                 .FirstOrDefaultAsync(c => c.Id == id);
 
-            if(model == null) return NotFound();
+            if (model == null) return NotFound();
 
             GerarLinks(model);
             return Ok(model);
@@ -53,7 +54,7 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult> Update(int id, Veiculo model)
         {
-            if(id != model.Id) return BadRequest();
+            if (id != model.Id) return BadRequest();
 
             var modeloDb = await _context.Veiculos.AsNoTracking()
                 .FirstOrDefaultAsync(c => c.Id == id);
@@ -86,6 +87,31 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "Delete"));
 
+        }
+
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int id, VeiculoUsuarios model)
+        {
+            if (id != model.VeiculoId) return BadRequest();
+            _context.VeiculosUsuarios.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.VeiculoId }, model);
+        }
+
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int id, int usuarioId)
+        {
+            var model = await _context.VeiculosUsuarios
+                .Where(c => c.VeiculoId == id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if (model == null) return NotFound();
+
+            _context.VeiculosUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
